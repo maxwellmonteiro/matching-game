6953801c3be003d6cb95e214d01e41f8e3ad9314
maxwell <maxwell.monteiro@gmail.com>
2021-07-08 17:57:31 -0300

    initial commit

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css"; // vite stuff, not vanilla js
+
+function CardGame() {           
+    return `
+        <article class="card-game">
+            <img src="src/images/alura-pixel.png">
+        </article>
+    `;    
+}
+
+export default CardGame;
